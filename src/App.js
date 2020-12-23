6d9074157af0662fe88c93337ea36c07636df495
ahotchkin <allyson.hotchkin@gmail.com>
2020-12-23 17:59:40 -0500

    First commit; Run create-react-app to setup frontend

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Investment Tracker Dashboard
         </p>
         <a
           className="App-link"
