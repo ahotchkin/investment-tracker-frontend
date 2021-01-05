7608d3b8ae9335e93c126dae302402a98e8f6063
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-04 22:03:23 -0500

    Import and render MainContainer, remove alll other rendered content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
+import MainContainer from './containers/MainContainer';
 
 function App() {
+
   return (
     <div className="App">
-      <p>
-        Investment Tracker Dashboard
-      </p>
-      <Login />
+      <MainContainer />
     </div>
   );
 }
