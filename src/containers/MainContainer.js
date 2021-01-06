416f3d7388b7bf54fcccb6e24e21160d996475bb
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 16:05:59 -0500

    Import SignUp component and uncomment code to render SignUp at /signup

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -8,7 +8,7 @@ import Login from '../components/Login';
 import NavBar from '../components/NavBar';
 import Dashboard from '../components/Dashboard';
 import Home from '../components/Home';
-
+import SignUp from '../components/SignUp'
 
 // change this to functional component with hooks - how????
 class MainContainer extends Component {
@@ -27,9 +27,7 @@ class MainContainer extends Component {
         <Switch>
           <Route exact path="/" render={ props => this.props.loggedIn ? <Dashboard /> : <Home /> } />
           <Route exact path="/login" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <Login history={props.history}/> } />
-          {/*
           <Route exact path="/signup" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <SignUp history={props.history}/> } />
-          */}
           <Route exact path="/logout" render={ () => <Redirect to="/" /> } />
 
         </Switch>
