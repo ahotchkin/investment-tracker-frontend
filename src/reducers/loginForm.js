14b56d0388671b70326605b63343a7d56625f048
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 16:06:22 -0500

    Add case for CLEAR_LOGIN_FORM

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,6 +7,8 @@ const loginFormReducer = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LOGIN_FORM":
       return action.formData
+    case "CLEAR_LOGIN_FORM":
+      return initialState
     default:
       return state
   }
