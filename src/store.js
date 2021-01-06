fcbdd406b238e93a047573702a8804762a03c691
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 16:07:29 -0500

    Import signUpForm reducer and add to combineRReducers

--- src/store.js
+++ src/store.js
@@ -3,10 +3,13 @@ import thunk from 'redux-thunk';
 
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
+import signUpForm from './reducers/signUpForm';
+
 // could separate this into a separate rootReducer file
 const reducer = combineReducers({
   currentUser,
   loginForm,
+  signUpForm,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
