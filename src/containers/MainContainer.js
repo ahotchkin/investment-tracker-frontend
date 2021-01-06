b505de25a8a6b3a861350350533bddcd7050ba53
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-05 20:12:04 -0500

    Import Route, Redirect, Switch, and necessary components, add conditionals to render, and routes to render, add mapStateToProps and pass as argument to connect

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import Login from '../components/Login';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
 import { getCurrentUser } from '../actions/currentUser';
 import { connect } from 'react-redux';
+
+import Login from '../components/Login';
+import NavBar from '../components/NavBar';
+import Dashboard from '../components/Dashboard';
+import Home from '../components/Home';
+
 
 // change this to functional component with hooks - how????
 class MainContainer extends Component {
@@ -13,17 +20,33 @@ class MainContainer extends Component {
   render() {
     return (
       <div className="App">
+        { this.props.loggedIn ? <NavBar /> : null }
         <p>
           Investment Tracker Dashboard
         </p>
-        <Login />
+        <Switch>
+          <Route exact path="/" render={ props => this.props.loggedIn ? <Dashboard /> : <Home /> } />
+          <Route exact path="/login" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <Login history={props.history}/> } />
+          {/*
+          <Route exact path="/signup" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <SignUp history={props.history}/> } />
+          */}
+          <Route exact path="/logout" render={ () => <Redirect to="/" /> } />
+
+        </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    loggedIn: !!state.currentUser,
+    // currentUser: state.currentUser,
+  }
+};
+
 const mapDispatchToProps = {
   getCurrentUser
 };
 
-export default connect(null, mapDispatchToProps)(MainContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
