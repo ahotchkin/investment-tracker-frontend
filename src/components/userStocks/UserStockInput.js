6b120f5977da289d2b7b25c37af5fbfa4ddd3b5d
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 22:31:04 -0500

    Add UserStockInput component to render form for user to add UserStock

---
+++ src/components/userStocks/UserStockInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const UserStockInput = props => {
+  return (
+    <form>
+    </form>
+  )
+}
+
+export default connect()(UserStockInput);
