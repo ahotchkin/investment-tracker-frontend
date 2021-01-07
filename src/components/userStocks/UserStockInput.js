53438a6ea06762d4055d8f92c97bf80c4c6c6008
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-07 13:08:59 -0500

    Begin to create form to add a UserStock, add mapDispatchToProps and mapStateToProps

--- src/components/userStocks/UserStockInput.js
+++ src/components/userStocks/UserStockInput.js
@@ -1,11 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Should this be here or in UserStockscontainer???
+import { updateUserStockForm } from '../actions/userStockForm';
+import { clearUserStockForm } from '../actions/userStockForm';
+// // These are attributes of a stock that need to be entered. A new stock will be created if it doesn't already exist.
+// name: "",
+// symbol: "",
+// industry: "",
+// sector: "",
+// // These are attributes of a user_stock that are specific to the current user.
+// purchaseDate: "",
+// numberOfShares: "",
+// totalSpent: ""
+
+const handleOnChange = () => {
+
+}
+
+const handleOnSubmit = () => {
+  console.log("You've submitted your userStock")
+}
+
 const UserStockInput = props => {
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={props.userStockFormData.name}
+        onChange={handleOnChange}
+      />
+
+      <br />
+
+      <label htmlFor="symbol">Symbol: </label>
+      <input
+        type="text"
+        name="symbol"
+        id="symbol"
+        value={props.userStockFormData.symbol}
+        onChange={handleOnChange}
+      />
+
+      <br />
+
+
     </form>
   )
 }
 
-export default connect()(UserStockInput);
+// should these be in UserStocksContainer?
+mapStateToProps = state => {
+  return {
+    userStockFormData: props.userStockForm
+  }
+}
+
+const mapDispatchToProps = {
+  updateUserStockForm,
+  clearUserStockForm
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserStockInput);
