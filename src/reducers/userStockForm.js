f09b4a967032e1e4375b227d47fa79e7f98ecddc
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-06 22:32:43 -0500

    Begin writing UserStockFormReducer

---
+++ src/reducers/userStockForm.js
@@ -0,0 +1,23 @@
+const initialState = {
+  purchaseDate: "",
+  numberOfShares: "",
+  totalSpent: "",
+  // t.integer "user_id", null: false
+  // t.integer "stock_id", null: false
+  //
+  //
+  // t.string "name"
+  // t.string "symbol"
+  // t.string "industry"
+  // t.string "sector"
+
+}
+
+
+
+
+onst userStockFormReducer = (state = initialState, acction) => {
+
+}
+
+export default userStockFormReducer
