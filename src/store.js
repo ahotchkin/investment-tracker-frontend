af770eb74271985a7969f089faa92ace1aa29e73
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-08 11:11:02 -0500

    Import stocks reducer and add to combineReduucers

--- src/store.js
+++ src/store.js
@@ -5,13 +5,15 @@ import signUpForm from './reducers/signUpForm';
 import loginForm from './reducers/loginForm';
 import currentUser from './reducers/currentUser';
 import userStockForm from './reducers/userStockForm';
+import stocks from './reducers/stocks';
 
 // could separate this into a separate rootReducer file
 const reducer = combineReducers({
   signUpForm,
   loginForm,
   currentUser,
-  userStockForm
+  userStockForm,
+  stocks
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
