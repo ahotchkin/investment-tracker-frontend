3869a94833b6750b151166442314cded5f3fe326
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-14 19:31:08 -0500

    Comment out fattributes in initialState that aren't needed at this time

--- src/reducers/userStockForm.js
+++ src/reducers/userStockForm.js
@@ -1,9 +1,10 @@
 const initialState = {
   // These are attributes of a stock that need to be entered. A new stock will be created if it doesn't already exist.
-  name: "",
-  symbol: "",
-  industry: "",
-  sector: "",
+  stock: "",
+  // Don't need the below right now - user can only select a stock that is already in the database
+  // symbol: "",
+  // industry: "",
+  // sector: "",
   // These are attributes of a user_stock that are specific to the current user.
   purchaseDate: "",
   numberOfShares: "",
