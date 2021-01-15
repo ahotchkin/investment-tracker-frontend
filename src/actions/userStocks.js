da217526a5f03bdd900b39e3dd71700c0b30cce0
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-14 20:00:00 -0500

    Write createUserStock function and fetch reqquest

--- src/actions/userStocks.js
+++ src/actions/userStocks.js
@@ -7,18 +7,32 @@ export const addUserStock = userStock => {
 }
 
 // asynchronous actions
-export const createUserStock = userStockData => {
-  // console.log(userStockData)
-  // const userStock = {
-  //   purchase_date: userStockData.purchaseDate,
-  //   number_of_shares: userStockData.numberOfShares,
-  //   total_spent: userStockData.totalSpent
-  // }
-  // return dispatch => {
-  //   fetch("http://localhost:3001/user_stocks/new", {
-  //     credentials: "include",
-  //     method: "POST",
-  //     body: JSON.stringify()
-  //   })
-  // }
+export const createUserStock = (userStockFormData, userId, stockId) => {
+  console.log(userStockFormData)
+  const userStock = {
+    purchase_date: userStockFormData.purchaseDate,
+    number_of_shares: userStockFormData.numberOfShares,
+    total_spent: userStockFormData.totalSpent,
+    user_id: userId,
+    stock_id: stockId,
+  }
+
+  return dispatch => {
+    fetch("http://localhost:3001/api/v1/user_stocks/new", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userStock)
+    })
+      .then(response => response.json)
+      .then(json => {
+        if (json.error) {
+          throw new Error(json.error)
+        } else {
+          dispatch(addUserStock(json.data))
+        }
+      })
+  }
 }
