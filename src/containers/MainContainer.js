4b1056c90093c9ed386e34399927e938810cf260
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-16 15:51:36 -0500

    Fix typo in render

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -31,7 +31,7 @@ class MainContainer extends Component {
           <Route exact path="/signup" render={ props => this.props.loggedIn ? <Redirect to="/" /> : <SignUp history={props.history}/> } />
 
           {/* Do I want this to go to /user_stocks or /user_stocks/new? */}
-          <Route exact path="/user_stocks/new" render={ routerProps => this.props.loggedIn ? <UserStocksContainer currentuser={this.props.currentUser} stocks={this.props.stocks} {...routerProps} /> : <Home /> } />
+          <Route exact path="/user_stocks/new" render={ routerProps => this.props.loggedIn ? <UserStocksContainer currentUser={this.props.currentUser} stocks={this.props.stocks} {...routerProps} /> : <Home /> } />
 
           <Route exact path="/stocks" render={ routerProps => this.props.loggedIn ? <StocksContainer stocks={this.props.stocks} {...routerProps} /> : <Home /> } />
 
