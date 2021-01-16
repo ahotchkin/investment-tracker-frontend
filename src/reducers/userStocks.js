088dfa17297eec6a26639a4bb63999696e82ee47
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-16 15:52:45 -0500

    Add case for ADD_USER_STOCK, add console.log in ADD_USER_STOCK case for debugging

--- src/reducers/userStocks.js
+++ src/reducers/userStocks.js
@@ -1,9 +1,13 @@
-const userStocksReducer = (state = [], action) {
+const userStocksReducer = (state = [], action) => {
   switch (action.type) {
-    case: "ADD_USER_STOCK"
+    case "SET_USER_STOCKS":
+      return action.userStocks
+    case "ADD_USER_STOCK":
       console.log(state.concat(action.userStock))
-      return state.concat(action.userStock)
-    default
+
+      // return state.concat(action.userStock)
+      return state
+    default:
       return state
   }
 }
