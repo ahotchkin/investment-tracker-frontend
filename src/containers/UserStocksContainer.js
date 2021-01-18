087aa2c8b7683139f553eac64cadc9161ce7a617
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-18 17:57:25 -0500

    Add commented code to render UserStocks at /user_stocks

--- src/containers/UserStocksContainer.js
+++ src/containers/UserStocksContainer.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import UserStockInput from '../components/userStocks/UserStockInput';
+// import UserStocks from '../components/userStocks/UserStocks'
 
 // update to functional component with hooks
 class UserStocksContainer extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div>
         <h3>I'm in the UserStocksContainer</h3>
         <Switch>
-          <Route exact path={this.props.match.url} render={ routerProps => <UserStockInput userId={this.props.currentUser.id} stocks={this.props.stocks} /> } />
+          <Route exact path={this.props.match.url} render={ routerProps => <UserStockInput userId={this.props.currentUser.id} stocks={this.props.stocks} history={this.props.history} /> } />
+
+          {/*
+          <Route exact path={this.props.match.url} render={ routerProps => <UserStocks userId={this.props.currentUser.id} stocks={this.props.stocks} /> } />
+          */}
         </Switch>
       </div>
     )
