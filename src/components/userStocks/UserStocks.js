f3cbcab1efc008c1363b4714aa69d017a217e717
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-20 20:53:09 -0500

    Add UserStockStock as prop for UserStockCard, render table with headers for each attribute of UserStockCard, render table body rows as userStockCards

--- src/components/userStocks/UserStocks.js
+++ src/components/userStocks/UserStocks.js
@@ -4,15 +4,43 @@ import { Link } from 'react-router-dom';
 import UserStockCard from './UserStockCard';
 
 const UserStocks = props => {
-  const userStockCards = props.userStocks.map(userStock => <UserStockCard userStock={userStock} key={userStock.id} />)
+
+  console.log(props)
+
+  // How do I call this in userStockCards when defining userStockStock prop?
+  // const userStockStock = (userStock) => {
+  //   props.stocks.filter(stock => stock.id === userStock.relationships.stock.data.id)
+  // }
+
+  const userStockCards = props.userStocks.map(userStock => <UserStockCard userStock={userStock} key={userStock.id} userStockStock={props.stocks.find(stock => stock.id === userStock.relationships.stock.data.id)}/>)
 
   return (
     <div>
       <h4>My Stocks</h4>
-      
-      <ul>
-        { userStockCards.length > 0 ? userStockCards : null }
-      </ul>
+
+        <table>
+          <thead>
+            <tr>
+              <td>Date Purchased</td>
+              <td>Days Held</td>
+              <td>Stock</td>
+              <td>Symbol</td>
+              <td>Industry</td>
+              <td>Sector</td>
+              <td>Purchase Cost</td>
+              <td>Stock Price</td>
+              <td>Number of Shares</td>
+              <td>Total Spent</td>
+              <td>Total Stock Value</td>
+              <td>% Return</td>
+              <td>Absolute $ Return</td>
+              <td>% Portfolio</td>
+            </tr>
+          </thead>
+          <tbody>
+            { userStockCards.length > 0 ? userStockCards : null }
+          </tbody>
+        </table>
 
       <Link to="/user_stocks/new">
         <button type="button">
