faf7b69a0ce74b986166cc4d804f24afe9a3aaa2
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-21 21:00:59 -0500

    Calculate Days Held and Purchase Cost, and format Total Spent in table

--- src/components/userStocks/UserStockCard.js
+++ src/components/userStocks/UserStockCard.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 const UserStockCard = props => {
   console.log(props)
+
+  const daysHeld = Math.floor((new Date().getTime() - Date.parse(props.userStock.attributes.purchase_date)) / (1000*60*60*24))
+
   return (
     <tr>
       <td>{props.userStock.attributes.purchase_date}</td>
-      <td># of days</td>
+      <td>{daysHeld}</td>
       <td>{props.userStockStock.attributes.name}</td>
       <td>{props.userStockStock.attributes.symbol}</td>
       <td>{props.userStockStock.attributes.industry}</td>
       <td>{props.userStockStock.attributes.sector}</td>
-      <td>$$$$$</td>
+      <td>${(props.userStock.attributes.total_spent / props.userStock.attributes.number_of_shares).toFixed(2)}</td>
       <td>Current stock price</td>
       <td>{props.userStock.attributes.number_of_shares}</td>
-      <td>{props.userStock.attributes.total_spent}</td>
+      <td>${parseFloat(props.userStock.attributes.total_spent).toFixed(2)}</td>
       <td>Stock value</td>
       <td>% return</td>
       <td>$ return</td>
