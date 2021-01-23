4fea42c90fd35ea19f00cd471697c064cf06c46f
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-23 12:39:16 -0500

    Add case for SET_STOCK_INFO - find stock in state and add stock info to object

--- src/reducers/stocks.js
+++ src/reducers/stocks.js
@@ -2,6 +2,14 @@ const stocksReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_STOCKS":
       return action.stocks
+    case "SET_STOCK_INFO":
+      return (state.map(stock => {
+        if (stock.id === action.stock.id) {
+          return Object.assign(stock, action.stock)
+        } else {
+          return stock
+        }
+      }))
     default:
       return state
   }
