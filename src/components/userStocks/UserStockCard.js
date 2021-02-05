63d2d6399c5a0b866732fd168b6fcdcc760908bd
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-05 13:35:42 -0500

    Update some info to placeholder to reduce API calls

--- src/components/userStocks/UserStockCard.js
+++ src/components/userStocks/UserStockCard.js
@@ -3,22 +3,24 @@ import React from 'react';
 const UserStockCard = props => {
 
   const daysHeld = Math.floor((new Date().getTime() - Date.parse(props.userStock.attributes.purchase_date)) / (1000*60*60*24))
-
-  const totalSpent = parseFloat(props.userStock.attributes.total_spent).toFixed(2)
-
-  const currentSharePrice = () => {
-    if (!!props.userStockStock["Meta Data"]) {
-      const lastRefreshed = props.userStockStock["Meta Data"]["3. Last Refreshed"]
-      const currentStockInfo = props.userStockStock["Time Series (5min)"][lastRefreshed]
-      const currentSharePrice = parseFloat(currentStockInfo["1. open"]).toFixed(2)
-      return currentSharePrice
-    }
-  }
-
-  const totalStockValue = (currentSharePrice() * props.userStock.attributes.number_of_shares).toFixed(2)
-
+  //
+  // const totalSpent = parseFloat(props.userStock.attributes.total_spent).toFixed(2)
+  //
+  // // currently causing error when userStockStock hasn't loaded yet
+  // const currentSharePrice = () => {
+  //   if (!!props.userStockStock["Meta Data"]) {
+  //     const lastRefreshed = props.userStockStock["Meta Data"]["3. Last Refreshed"]
+  //     const currentStockInfo = props.userStockStock["Time Series (5min)"][lastRefreshed]
+  //     const currentSharePrice = parseFloat(currentStockInfo["1. open"]).toFixed(2)
+  //     return currentSharePrice
+  //   }
+  // }
+  //
+  // const totalStockValue = (currentSharePrice() * props.userStock.attributes.number_of_shares).toFixed(2)
+  //
   return (
     <tr>
+      {/*
       <td>{props.userStock.attributes.purchase_date}</td>
       <td>{daysHeld}</td>
       <td>{props.userStockStock.attributes.name}</td>
@@ -33,6 +35,22 @@ const UserStockCard = props => {
       <td>{((totalStockValue / totalSpent) - 1).toFixed(2)}</td>
       <td>${(totalStockValue - totalSpent).toFixed(2)}</td>
       <td>% portfolio</td>
+      */}
+
+      <td>{props.userStock.attributes.purchase_date}</td>
+      <td>{daysHeld}</td>
+      <td>{props.userStockStock.attributes.name}</td>
+      <td>{props.userStockStock.attributes.symbol}</td>
+      <td>{props.userStockStock.attributes.industry}</td>
+      <td>{props.userStockStock.attributes.sector}</td>
+      <td>current share price here</td>
+      <td>{props.userStock.attributes.number_of_shares}</td>
+      <td>$ total spent</td>
+      <td>$ total stock value</td>
+      <td>% return here</td>
+      <td>$ return here</td>
+      <td>% portfolio</td>
+
     </tr>
   )
 }
