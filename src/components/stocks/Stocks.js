baf4cc78c45d8b2ad798a7771240251b825a115d
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-12 10:23:39 -0500

    Add link to stocks page to add a stock to the database

--- src/components/stocks/Stocks.js
+++ src/components/stocks/Stocks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import StockCard from './StockCard';
 
@@ -12,6 +13,11 @@ const Stocks = props => {
       <ul>
         { stockCards.length > 0 ? stockCards : null }
       </ul>
+      <Link to="/stocks/new">
+        <button type="button">
+          Add Stock to Database
+        </button>
+      </Link>
     </div>
   )
 }
