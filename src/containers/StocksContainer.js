34b931347348221298646d654f947cf3dacf34d3
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-12 10:25:24 -0500

    Import connect, StockInput, and all actions relating to createStock

--- src/containers/StocksContainer.js
+++ src/containers/StocksContainer.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
 import Stocks from '../components/stocks/Stocks';
+import StockInput from '../components/stocks/StockInput';
+
+import { updateStockForm } from '../actions/stockForm';
+import { clearStockForm } from '../actions/stockForm';
+import { createStock } from '../actions/stocks';
 
 class StocksContainer extends Component {
   render() {
@@ -11,6 +16,7 @@ class StocksContainer extends Component {
         <h3>I'm in the StocksContainer</h3>
         <Switch>
           <Route exact path={this.props.match.url} render={ routerProps => <Stocks stocks={this.props.stocks} /> } />
+          <Route exact path={`${this.props.match.url}/new`} render={ routerProps => <StockInput updateStockForm={this.props.updateStockForm} clearStockForm={this.props.clearStockForm} createStock={this.props.createStock} /> } />
         </Switch>
       </div>
     )
@@ -24,5 +30,6 @@ class StocksContainer extends Component {
 //   }
 // }
 
-// export default connect(null)(StocksContainer);
-export default StocksContainer;
+
+
+export default connect(null, { updateStockForm, clearStockForm, createStock })(StocksContainer);
